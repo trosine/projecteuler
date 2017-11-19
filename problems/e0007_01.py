cff8ffebefd7eae318d5516f64c3b148fe28427e
Tim Rosine <tim.rosine@gmail.com>
2017-11-19 00:59:49 -0600

    Solve problem 7
    I had already effectively solved this before by providing getitem of my
    iterable primes class.
    
    The overview did provide an interesting trivial, yet efficient primality
    test:
    if n == 1: return False
    elif n < 4: return True
    elif not n % 2: return False
    elif n < 9: return True  # primes 5,7 since 4,6,8 already excluded
    else: for (f=5; f < sqrt(n); f+=6): test n%f and n%f+2

---
+++ problems/e0007_01.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""Project Euler: 0007
+
+https://projecteuler.net/problem=7
+
+10001st prime
+
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that
+the 6th prime is 13.
+
+What is the 10 001st prime number?
+"""
+
+import math
+import sys
+
+PROBLEM = 7
+SOLVED = True
+SPEED = 0.70
+TAGS = ['prime']
+
+class Primes(object):
+    """Iteratable class that handles prime number generation and testing"""
+
+    # cache of currently known primes
+    known_primes = [2, 3]
+
+    def __init__(self, maximum=float('inf'), count=float('inf')):
+        self.maximum = maximum
+        self.count = count
+        self.__iter = 0
+
+    def __iter__(self):
+        return self
+
+    def __getitem__(self, key):
+        if not isinstance(key, int):
+            raise TypeError('Cannot use "%s" as a list index' % type(key))
+        while len(self.known_primes) <= key:
+            self.next()
+        return self.known_primes[key]
+
+    def next(self):
+        """Fetch the next prime number"""
+        if self.__iter >= self.count:
+            # print 'Reached maximum count %d (%d)' % (self.count, self.__iter)
+            raise StopIteration()
+        if self.__iter < len(self.known_primes):
+            if self.known_primes[self.__iter] > self.maximum:
+                raise StopIteration()
+            key = self.__iter
+            self.__iter += 1
+            return self.known_primes[key]
+        candidate = self.known_primes[-1] + 2
+        while True:
+            # print 'Checking to see if candidate %d is prime' % candidate
+            if candidate > self.maximum:
+                raise StopIteration()
+            if not self.first_factor(candidate):
+                self.known_primes.append(candidate)
+                self.__iter += 1
+                return candidate
+            candidate += 2
+
+    @classmethod
+    def first_factor(cls, number):
+        """Returns the lowest factor of the number.
+        If the number is prime, None is returned instead.
+        """
+        for prime in cls(maximum=math.sqrt(number)):
+            if not number % prime:
+                return prime
+        return None
+
+    @classmethod
+    def factor(cls, number):
+        """Returns a list of prime factors that this number is composed of"""
+        factors = []
+        for prime in cls():
+            # print 'Checking to see if %d is a factor of %d' % (prime, number)
+            if prime == number:
+                factors.append(prime)
+                break
+            # reduce the total iterations
+            if prime > math.sqrt(number):
+                factors.append(number)
+                break
+            while not number % prime:
+                number /= prime
+                factors.append(prime)
+                # case for squares (which are divisible)
+                if prime == number:
+                    factors.append(prime)
+                    break
+        return factors
+
+
+
+def main():
+    """Solve problem."""
+    print 'Project Euler: %04d' % PROBLEM
+    prime = 10000
+    if len(sys.argv) > 1:
+        prime = int(sys.argv[1])
+    primes = Primes()
+    print primes[prime]
+
+
+if __name__ == '__main__':
+    main()
